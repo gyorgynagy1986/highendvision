c35dc4b080578117d9376a23983b7378351a4cab
Nagy Gyorgy <gyorgynagy1986@gmail.com>
2024-05-28 19:50:28 +0200

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,37 @@
+"use client"
+
+import React, { useState, useEffect } from 'react';
+
+import Image from "next/image";
+import styles from "./page.module.css";
+import Logo from "../../public/assets/logo.svg";
+import Dark from "../../public/assets/dark.svg";
+
+export default function Home() {
+
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const hour = new Date().getHours();
+    if (hour >= 7 && hour < 20) {
+      setIsDarkMode(false);
+    } else {
+      setIsDarkMode(true);
+    }
+  }, []);
+
+  return (
+    <main className={`${styles.main} ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
+      <div className={styles.container}>
+       {isDarkMode ? <Image priority className={styles.logo} src={Dark} /> : <Image priority className={styles.logo} src={Logo} /> }
+        <div className={styles.textContainer}>
+        <h1 >
+          We are a boutique design agency crafting bespoke online experiences
+          for exquisite clients.
+        </h1>
+         <a href="mailto:get@highendvisions.com">get@highendvisions.com</a>
+        </div>
+      </div>
+    </main>
+  );
+}
